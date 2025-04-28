47398e2cce0114c9d0730d131b41117f1fbe6baa
Jadiefication <jadeczjade008@gmail.com>
2025-04-28 15:45:49 +0200

    refactor: move to opengl

--- src/main/kotlin/io/insequence/renderer/Renderer.kt
+++ src/main/kotlin/io/insequence/renderer/Renderer.kt
@@ -1,30 +1,23 @@
 package io.insequence.renderer
 
-import io.insequence.exception.ExceptionHandler
 import io.insequence.renderer.exception.GLFWInitializeException
-import io.insequence.renderer.exception.WindowCreationException
-import org.lwjgl.PointerBuffer
-import org.lwjgl.glfw.GLFW.*
-import org.lwjgl.glfw.GLFWVulkan.Functions.VulkanSupported
-import org.lwjgl.glfw.GLFWVulkan.glfwCreateWindowSurface
-import org.lwjgl.glfw.GLFWVulkan.glfwGetRequiredInstanceExtensions
-import org.lwjgl.glfw.GLFWVulkan.glfwVulkanSupported
-import org.lwjgl.system.MemoryStack
+import org.lwjgl.glfw.GLFW.GLFW_PLATFORM
+import org.lwjgl.glfw.GLFW.GLFW_PLATFORM_X11
+import org.lwjgl.glfw.GLFW.glfwCreateWindow
+import org.lwjgl.glfw.GLFW.glfwDestroyWindow
+import org.lwjgl.glfw.GLFW.glfwInit
+import org.lwjgl.glfw.GLFW.glfwInitHint
+import org.lwjgl.glfw.GLFW.glfwPlatformSupported
+import org.lwjgl.glfw.GLFW.glfwPollEvents
+import org.lwjgl.glfw.GLFW.glfwTerminate
+import org.lwjgl.glfw.GLFW.glfwWindowShouldClose
 import org.lwjgl.system.MemoryUtil.NULL
-import org.lwjgl.vulkan.*
-import org.lwjgl.vulkan.KHRSurface.vkDestroySurfaceKHR
-import org.lwjgl.vulkan.VK10.*
-import org.lwjgl.vulkan.VK13.VK_API_VERSION_1_3
-import java.nio.ByteBuffer
-import java.nio.LongBuffer
 import kotlin.properties.Delegates
 
 
 class Renderer {
 
     var window by Delegates.notNull<Long>()
-    private lateinit var vkInstance: VkInstance
-    private var surfacePtr by Delegates.notNull<Long>()
 
     fun openWindow(windowStats: Window) {
         if (glfwPlatformSupported(GLFW_PLATFORM_X11)) {
@@ -34,41 +27,8 @@ class Renderer {
         if (!glfwInit()) {
             throw GLFWInitializeException()
         }
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API)
-        MemoryStack.stackPush().use {
-            val info: VkApplicationInfo = VkApplicationInfo.calloc(it).apply {
-                sType(VK_STRUCTURE_TYPE_APPLICATION_INFO)
-                pApplicationName(it.UTF8(windowStats.name))
-                applicationVersion(VK_MAKE_VERSION(1, 0, 0))
-                apiVersion(VK_API_VERSION_1_3)
-            }
-
-            val createInfo = VkInstanceCreateInfo.calloc(it).apply {
-                sType(VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO)
-                pApplicationInfo(info)
-                ppEnabledExtensionNames(glfwGetRequiredInstanceExtensions())
-            }
-
-            val pInstance = it.mallocPointer(1)
-
-            if (vkCreateInstance(createInfo, null, pInstance) != VK_SUCCESS) {
-                throw WindowCreationException(window = windowStats)
-            }
-
-            // Retrieve instance handle
-            val instance = pInstance.get(0)
-            vkInstance = VkInstance(instance, createInfo)
-
-            println("Vulkan Instance Created Successfully: $instance")
-        }
 
         window = glfwCreateWindow(windowStats.width, windowStats.height, windowStats.name, NULL, NULL)
-        surfacePtr = MemoryStack.stackPush().use {
-            val pSurface = it.mallocLong(1)
-            if (glfwCreateWindowSurface(vkInstance, window, null, pSurface) != VK_SUCCESS)
-                throw RuntimeException("Failed to create Vulkan surface")
-            pSurface[0]
-        }
     }
 
     fun mainLoop(function: () -> Unit) {
@@ -76,8 +36,6 @@ class Renderer {
             glfwPollEvents()
             function()
         }
-        vkDestroySurfaceKHR(vkInstance, surfacePtr, null)
-        vkDestroyInstance(vkInstance, null)
         glfwDestroyWindow(window)
         glfwTerminate()
     }
